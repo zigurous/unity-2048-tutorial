830a77a380f0db4f63205ea2c5dc8cdeb60c76fa
Adam Graham <adam@zigurous.com>
2023-11-11 19:35:31 -0600

    Rename GameBoard to TileBoard

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public GameBoard board;
+    public TileBoard board;
     public CanvasGroup gameOver;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI hiscoreText;
